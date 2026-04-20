a45a586966a284ac5d377e833653884f3bca9a19
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2026-04-20 14:44:50 +0200

    add style.css for improved styling and layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "../api/public/style.css";
+import "./style.css";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
